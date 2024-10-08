b20fe9024fbfa6bff379a192c6e0220bf1b92453
who-else-but-arjun <152922576+who-else-but-arjun@users.noreply.github.com>
2024-10-08 11:42:07 +0530

    Update faceCrop.py

--- faceCrop.py
+++ faceCrop.py
@@ -2,12 +2,10 @@ import cv2
 import os
 from pathlib import Path
 
-# Load the pre-trained Haar Cascade model for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-
 def extract_faces(video_path):
     video_name = Path(video_path).stem
-    output_dir = "faces"
+    output_dir = f"faces"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -33,17 +31,15 @@ def extract_faces(video_path):
             aspect_ratio = w / float(h)
             if 0.8 < aspect_ratio < 1.2: 
                 face = frame[y:y+h, x:x+w]
-                face_resized = cv2.resize(face, (224, 224))
-                
                 face_image_path = os.path.join(output_dir, f"face_{frame_count}_{face_count}.jpg")
-                cv2.imwrite(face_image_path, face_resized) 
+                cv2.imwrite(face_image_path, face)
                 print(f"Saved: {face_image_path}")
                 face_count += 1
 
         frame_count += 1
     
-    cap.release()
-    cv2.destroyAllWindows()
+    #cap.release()
+    #cv2.destroyAllWindows()
     return output_dir
 
 if __name__ == "__main__":
